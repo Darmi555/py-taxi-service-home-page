3678ab4726c5eaec9cb7b46960cd4c2fca642d6c
Darmi555 <mateusz.rudnicki05@wp.pl>
2026-03-26 13:52:04 +0100

    donex2

--- taxi/views.py
+++ taxi/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
-from .models import Driver, Manufacturer, Car
 
-# Create your views here.
+from taxi.models import Driver, Manufacturer, Car
+
+
 def index(request: HttpRequest) -> HttpResponse:
     num_drivers = Driver.objects.count()
     num_manufacturers = Manufacturer.objects.count()
